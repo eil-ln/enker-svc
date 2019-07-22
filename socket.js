36af4ea61fafb8cd5e4160c34b4110e7869487d0
eil-ln <36309203+eil-ln@users.noreply.github.com>
2019-07-22 18:53:47 +0400

    logout functionality

--- socket.js
+++ socket.js
@@ -21,13 +21,67 @@ function usersNamespace(io) {
     // TODO: add listener for drawing
 
     // TODO: add listener for logging in, update flag loggedIn in Database, join room
+    socket.on('login', user => {
+      socket.join(user.email);
+
+      db.getClient().collection("students").findOneAndUpdate(
+        {email: user.email},
+        {$set: {'loggedIn': true}},
+        {returnOrigignal: false},
+        function(err, results) {
+          if (err) {
+            socket.emit('list error', err);
+          } else if (results.value == null) {
+            socket.emit('list error', {error: "Student with email " })
+          } else {
+            users.emit('logged in', results.value);
+          }
+        }
+      );
+    })
 
     // TODO: add listener on 'disconnect' to log out user, and emit
+    socket.on('disconnect', user => {
+      socket.leave(user.email);
 
+      db.getClient().collection("students").findOneAndUpdate(
+        {email: user.email},
+        {$set: {'loggedIn': false}},
+        {returnOrigignal: false},
+        function(err, results) {
+          if (err) {
+            socket.emit('list error', err);
+          } else if (results.value == null) {
+            socket.emit('list error', {error: "Student with email " })
+          } else {
+            users.emit('disconnected', results.value);
+          }
+        }
+      );
+    })
     // TODO: add listener for logout message, update db, emit
-    
-    // TODO: add listener to search query
+    socket.on('logout', user => {
+      socket.leave(user.email);
+      console.log("logged out");
+
+      db.getClient().collection("students").findOneAndUpdate(
+        {email: user.email},
+        {$set: {'loggedIn': false}},
+        {returnOrigignal: false},
+        function(err, results) {
+          if (err) {
+            socket.emit('list error', err);
+          } else if (results.value == null) {
+            socket.emit('list error', {error: "Student with email " })
+          } else {
+            users.emit('logged out', results.value);
+          }
+        }
+      );
+    })
   });
+
+  // TODO: add listener to search query
 }
 
 module.exports = {
